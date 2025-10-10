83fc7b665c902c32a2169442749d96b74a2253ab
AMADOU-KOUNTCHE Djibrilla <djibrilla.amadou-kountche@akka.eu>
2025-10-10 16:11:10 +0200

    Refactor code in the license controller

--- src/apiserver/src/openapi_server/controllers/license_controller.py
+++ src/apiserver/src/openapi_server/controllers/license_controller.py
@@ -6,6 +6,8 @@ from openapi_server.models.license_data import LicenseData
 from openapi_server import util
 
 from pymongo import MongoClient
+from flask import current_app, g
+
 import os
 
 host     = os.environ['MONGODB_HOST']
@@ -13,8 +15,11 @@ username = os.environ['MONGODB_USER']
 password = os.environ['MONGODB_PASSWORD']
 database = os.environ['LICENSE_DB_NAME']
 
-client = MongoClient(host, username=username, password=password, authSource=database) # Update if using remote MongoDB instance
-db = client[database]
+def get_db():
+    if 'mongodb' not in g:
+        client = MongoClient(host=host, username=username, password=password, authSource=database) # Update if using remote MongoDB instance
+        g.mongodb = client[database]
+    return g.mongodb
 
 def delete_license(license_id):  # noqa: E501
     """Delete a specific license
@@ -26,8 +31,8 @@ def delete_license(license_id):  # noqa: E501
 
     :rtype: None
      """
-    if len(list(db['licenses'].find({'_id': license_id}))) > 0:
-        db['licenses'].delete_one({'_id': license_id})
+    if len(list(get_db()['licenses'].find({'_id': license_id}))) > 0:
+        get_db()['licenses'].delete_one({'_id': license_id})
         return 'License deleted', 200
     else:
         return "License not found (ID does not exist)", 404
@@ -42,8 +47,8 @@ def get_license(license_id):  # noqa: E501
 
     :rtype: License
     """
-    if len(list(db['licenses'].find({'_id': license_id}))) > 0:
-        return db['licenses'].find_one({'_id': license_id}), 200
+    if len(list(get_db()['licenses'].find({'_id': license_id}))) > 0:
+        return get_db()['licenses'].find_one({'_id': license_id}), 200
     else:
         return "License not found (ID does not exist)", 404
 
@@ -55,10 +60,12 @@ def get_licenses():  # noqa: E501
 
     :rtype: None
     """
-    return {
-        'licenses': list(db['licenses'].find()),
-        'total_records': len(list(db['licenses'].find()))
-    }, 200
+    licences = list(get_db()['licenses'].find())
+    licences = {
+        'licenses': licences,
+        'total_records': len(licences)
+    }
+    return licences, 200
 
 
 def post_licenses(body):  # noqa: E501
@@ -75,8 +82,8 @@ def post_licenses(body):  # noqa: E501
         
         body = License.from_dict(body)  # noqa: E501
 
-    if len(list(db['licenses'].find({'_id': body.id}))) == 0:
-        db['licenses'].insert_one({'_id': body.id, 'license': str(body.license)})
+    if len(list(get_db()['licenses'].find({'_id': body.id}))) == 0:
+        get_db()['licenses'].insert_one({'_id': body.id, 'license': str(body.license)})
         return "License added with success", 200
     else:
         return "License already exists with this ID", 400
@@ -97,8 +104,8 @@ def put_license(body, license_id):  # noqa: E501
     """
     if connexion.request.mimetype == "application/json":
         body = License.from_dict(body)  # noqa: E501
-    if len(list(db['licenses'].find({'_id': license_id}))) > 0:
-        db['licenses'].update_one({'_id': license_id}, {'$set': {'_id': license_id, 'license': str(body)}})
+    if len(list(get_db()['licenses'].find({'_id': license_id}))) > 0:
+        get_db()['licenses'].update_one({'_id': license_id}, {'$set': {'_id': license_id, 'license': str(body)}})
         return "License update with success", 200
     else:
         return "License not found (ID does not exist)", 404
